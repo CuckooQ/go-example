eac8709ac463d53c1ab1c6950a91fa6156dd6908
jaeyong4536 <jaeyong4536@gmail.com>
2020-05-02 18:30:06 +0900

    go example

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/cuckooq/learngo/something"
 )
 
 func multiply(a int, b int) int {
 	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
@@ -21,5 +30,14 @@ func main() {
 	fmt.Println(name2)
 	fmt.Println(name3)
 	something.SayBye()
-	fmt.Println(multiply(2, 2))
+	fmt.Println(multiply(3, 2))
+
+	totalLengh, uppername := lenAndUpper("name")
+	fmt.Println(totalLengh)
+	fmt.Println(uppername)
+
+	totalLenghRe, _ := lenAndUpper("test")
+	fmt.Println(totalLenghRe)
+
+	repeatMe("name", "name2")
 }
